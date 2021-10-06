c720441d3cabea62933fa24d5d576d1b3d6072e9
Arlony Diaz <arlonydiaz@gmail.com>
2021-10-06 17:18:11 -0500

    add body scroll lock when modal is open

--- src/js/productDetailModal.js
+++ src/js/productDetailModal.js
@@ -1,22 +1,25 @@
 let sisezModal = document.getElementById("sizes-modal");
 let openModalBtn = document.getElementById("open-modal");
 let closeModalBtn = document.getElementById("close-modal");
+let bodyElem = document.body;
 
 // check if items exist before calling any function
 // to avoid null or undefined errors
-if(sisezModal && openModalBtn && closeModalBtn) {
+if (sisezModal && openModalBtn && closeModalBtn) {
   openModalBtn.onclick = function () {
+    bodyElem.style.overflow = "hidden";
     sisezModal.style.display = "flex";
   };
 
-  closeModalBtn.onclick = function() {
+  closeModalBtn.onclick = function () {
+    bodyElem.style.overflow = "auto";
     sisezModal.style.display = "none";
-  }
+  };
 
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (sisezModal && event.target === sisezModal) {
+      bodyElem.style.overflow = "auto";
       sisezModal.style.display = "none";
     }
-  }
+  };
 }
-
